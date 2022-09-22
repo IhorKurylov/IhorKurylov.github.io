b47bbb854a2eb94fd60fa4537f1378c0fd07fa77
IhorKurylov <idSNrY7I3xhB>
2022-09-22 22:41:35 +0300

    fixes for gh pages

--- todo/src/index.tsx
+++ todo/src/index.tsx
@@ -11,9 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={AppRoutes.TODO}>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
